7de636236db94862dddd2eb405b29f890253e4c6
phaneendra999 <phanindrasangoju999@gmail.com>
2020-07-14 08:36:57 +0530

    Jul 14  08:36:57

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -12,3 +12,11 @@ def fun_getinrange(x, bound1, bound2):
 		elif(x > bound2):
 			return bound2
 		return x
+	else:
+		if(x < bound1 and x > bound2):
+			return x
+		elif(x > bound1):
+			return bound1
+		elif(x < bound2):
+			return bound2
+	return -1
