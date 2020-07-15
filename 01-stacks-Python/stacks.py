a30a5333f30afe76ce3f083e92a62eb84cdd2e5a
phaneendra999 <phanindrasangoju999@gmail.com>
2020-07-15 17:35:03 +0530

    Jul 15  17:35:03

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -26,11 +26,19 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        pass
+        if(self.head):
+            new_element.next = self.head
+            self.head = new_element
+        else:
+            self.head = new_element
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        pass
+        if(self.head):
+            poppedElement = current
+            self.head = current.next
+            return poppedElement
+        return None
 
 class stack(object):
     def __init__(self,top=None):
@@ -38,9 +46,9 @@ class stack(object):
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        pass
+        self.li.insert_first(new_element)
 
     def pop(self):
         "Pop (remove) the first element off the top of the stack and return it"
-        pass
+        return self.li.delete_first()
     
