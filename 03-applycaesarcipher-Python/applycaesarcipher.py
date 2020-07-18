4c0b50c13f45a465ff39621e4392d63e065f4229
phaneendra999 <phanindrasangoju999@gmail.com>
2020-07-18 20:59:54 +0530

    Jul 18  20:59:54

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -11,7 +11,15 @@
 
 
 def fun_applycaesarcipher(msg, shift):
-	return ""
+	str1 = ""
+	for i in range(len(msg)):
+		char = msg[i]
+		if ord(char) == 32:
+			str1 += chr(32)
+		elif(char.isupper()):
+			str1 += chr((ord(char) + shift -65) % 26 + 65)
+		str1 += chr((ord(char) + shift - 97) % 26 + 97)
+	return str1
 
 
 
