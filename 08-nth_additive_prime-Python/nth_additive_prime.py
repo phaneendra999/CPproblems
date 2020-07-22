5ea1b11d00eac72fd9fb111327ffcdf172b840a1
phaneendra999 <phanindrasangoju999@gmail.com>
2020-07-22 13:07:54 +0530

    done

--- 08-nth_additive_prime-Python/nth_additive_prime.py
+++ 08-nth_additive_prime-Python/nth_additive_prime.py
@@ -4,7 +4,26 @@
 # is also prime, so 113 is an Additive Prime. fun_nth_additive_prime(0) returns 2
 
 
-
-
+all_primes = []
+additive_primes = []
 def fun_nth_additive_prime(n):
-	return 1
+	count = 0
+	start_prime = 2
+	while count <= n:
+		flag = False
+		for i in all_primes:
+			if(start_prime % i) == 0:
+				flag = True
+		if(not flag):
+			all_primes.append(start_prime)
+			if(get_sum(start_prime) in all_primes):
+				additive_primes.append(start_prime)
+				count += 1
+		start_prime += 1
+	return additive_primes[n]
+def get_sum(n):
+	sum = 0
+	while n != 0:
+		sum += n%10
+		n = n//10
+	return sum
