386e92f4536b2749fc77c1685e7c705438055f7d
phaneendra999 <phanindrasangoju999@gmail.com>
2020-07-23 09:34:52 +0530

    Jul 23  09:34:52

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -6,7 +6,19 @@
 # lookAndSay([-1,2,7]) == [(1,-1),(1,2),(1,7)]
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
-
+import math
 def lookandsay(a):
 	# Your code goes here
-	pass
+	if(not len(a)):
+		return []
+	temp = math.inf
+	count = 0
+	res = []
+	for i in a:
+		if(i != temp):
+			if(temp != math.inf):
+				res.append((count,temp))
+			temp = i
+			count = 1
+		count += 1
+	return res
