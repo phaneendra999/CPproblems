5138be747c6dffc320b9302542bd5ab84847e5fe
phaneendra999 <phanindrasangoju999@gmail.com>
2020-07-26 10:36:15 +0530

    done

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -4,6 +4,22 @@
 # run, or the smallest such digit if there is a tie. So, longestDigitRun(117773732) returns 7 (
 # because there is a run of 3 consecutive 7's), 
 # as does longestDigitRun(-677886).
+def lookandsay(a):
+	c = 1
+	l =[]
+	for i in range(1,len(a)+1):
+		if(i<len(a) and a[i-1] == a[i]):
+			c +=1
+		l.append((c,a[i-1]))
+		c=1
+	return(l)
 def longestdigitrun(n):
 	# Your code goes here
-	pass
+	n = lookandsay(list(map(int,str(abs(n)))))
+	a = b = 0
+	for i in n:
+		if(i[0] == a and i[1]<b):
+			b = i[1]
+		elif(i[0] > a):
+			a,b = i[0],i[1]
+	return b
